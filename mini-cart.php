f151b3db5e1fd7eba137c5bcf45f8729d9d6e8aa
Sabirul Islam <45106497+mdsabir07@users.noreply.github.com>
2019-12-25 12:26:13 +0600

    commit

--- mini-cart.php
+++ mini-cart.php
@@ -164,3 +164,23 @@ add_filter( 'woocommerce_add_to_cart_fragments', 'wnew_shop_woocommerce_cart_lin
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+// Mini cart use by shortcode. First Create the shortcode on functions.php
+function custom_mini_cart() { 
+	echo '<a href="#" class="dropdown-back" data-toggle="dropdown"> ';
+	echo '<i class="fa fa-shopping-cart" aria-hidden="true"></i>';
+	echo '<div class="basket-item-count" style="display: inline;">';
+	echo '<span class="cart-items-count count">';
+	echo WC()->cart->get_cart_contents_count();
+	echo '</span>';
+	echo '</div>';
+	echo '</a>';
+	echo '<ul class="dropdown-menu dropdown-menu-mini-cart">';
+	echo '<li> <div class="widget_shopping_cart_content">';
+	woocommerce_mini_cart();
+	echo '</div></li></ul>';
+}
+add_shortcode( 'custom-techno-mini-cart', 'custom_mini_cart' );
+?>
+// Now add this inside the template
+ <?php echo do_shortcode(['custom-techno-mini-cart']); ?>
